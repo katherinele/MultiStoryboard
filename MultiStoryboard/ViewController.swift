3ac27a9b00407ef42fc2e201f49c611d212d75c0
soupsueapple <soupsueapple@gmail.com>
2015-07-08 09:26:48 +0800

    Initial Commit

---
+++ MultiStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiStoryboard
+//
+//  Created by 汤坤 on 15/7/8.
+//  Copyright (c) 2015年 Soup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
